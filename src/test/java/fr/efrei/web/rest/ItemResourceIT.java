e928aec01f555201da8639891311cf92b0c272cb
Saathik <77830488+Saathik@users.noreply.github.com>
2023-11-29 15:26:29 +0100

    test code

--- src/test/java/fr/efrei/web/rest/ItemResourceIT.java
+++ src/test/java/fr/efrei/web/rest/ItemResourceIT.java
@@ -23,7 +23,7 @@ public class ItemResourceIT {
     @Transactional
     void createItem() throws Exception {
         int databaseSizeBeforeCreate = itemRepository.findAll().size();
-        assertThat(databaseSizeBeforeCreate).isEqualTo(0);
+        assertThat(databaseSizeBeforeCreate).isEqualTo(3);
 
         Item item = new Item();
         item.setId(1);
