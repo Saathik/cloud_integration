ea536201417d3f283ee09a95c00ca0b256929284
Saathik <77830488+Saathik@users.noreply.github.com>
2023-11-29 15:21:43 +0100

    fix test

--- src/test/java/fr/efrei/BackendApplicationTests.java
+++ src/test/java/fr/efrei/BackendApplicationTests.java
@@ -2,8 +2,11 @@ package fr.efrei;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+		locations = "classpath:application-test.properties")
 class BackendApplicationTests {
 
 	@Test
