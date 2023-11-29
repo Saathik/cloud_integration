7a1ee08497a8addbf95cc6a5258264d02f20e137
Saathik <77830488+Saathik@users.noreply.github.com>
2023-11-29 15:14:06 +0100

    remove useless file

--- build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO item (id, name, price) VALUES (1, 'Shoes', 60.00);
-INSERT INTO item (id, name, price) VALUES (2, 'T-Short', 20.00);
-INSERT INTO item (id, name, price) VALUES (3, 'Jeans', 40.00);
-INSERT INTO item (id, name, price) VALUES (4, 'Sweater', 40.00);
-INSERT INTO item (id, name, price) VALUES (5, 'Hat', 15.00);
